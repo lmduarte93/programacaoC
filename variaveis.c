3002e6e5c5c4d63308d091f6349fc1601907cbfd
lmduarte93 <lucas.m.duarte@hotmail.com>
2025-02-18 23:44:48 -0300

    Update variaveis.c

--- variaveis.c
+++ variaveis.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    /*Em um mesmo int posso declarar idade e matricula, pois são números inteiros*/
+
+    printf("Digite sua idade: \n");
+    scanf("%d", idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%.2f", altura);
+
+    printf("Digite o seu nome: \n");
+    scanf("%s", nome);
+
+    printf("Digite a sua matricula: \n");
+    scanf("%d", matricula);
+
+    printf("Nome: %s - Idade: %d\n", nome, idade);
+    printf("Matricula: %d - Altura: %.2f", matricula, altura);
